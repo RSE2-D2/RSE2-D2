10131459f9f825b73306ed1bdd254ac5a6a2d4ef
Blair Archibald <mrblairarchibald@gmail.com>
2020-04-02 12:27:48 +0100

    Fix tests

--- bots/test_analyseGithub.py
+++ bots/test_analyseGithub.py
@@ -11,9 +11,5 @@ def test_containsGithubURL_url():
     assert analyseGithub.containsGitHubURL(repo)
 
 def test_extractGitHubLink():
-<<<<<<< HEAD
-    assert analyseGithub.extractGitHubLink("https://github.com/git/git more tweet") == "git/git"
-=======
-    repo = "https://github.com/git/git"
-    assert analyseGithub.extractGitHubLink(f"{repo} more tweet") == repo
->>>>>>> 4e7007bb958a62bf9483ff7f77c3641d2482f3e9
+    repo = "git/git"
+    assert analyseGithub.extractGitHubLink("https://github.com/{repo} more tweet") == repo
